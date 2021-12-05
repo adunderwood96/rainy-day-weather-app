c85d11b1ccadefdf6068385dcc4559369a2bece0
Ashley <adunderwood96@gmail.com>
2021-12-05 16:48:10 -0500

    working on current day forecast api

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,14 +8,14 @@ else
     searchHistory = [];
 
 var searchBtn = document.getElementById("searchBtn");
-searchBtn.addEventListener("click", searchResults);
+searchBtn.addEventListener("click", handleSearchResults);
 
 searchList();
 
-function searchResults() {
+function handleSearchResults() {
     var search = document.getElementById("search").value;
     if (document.getElementById("search").value !== "") {
-
+// Get weather results on search and save city
         currentWeather(search);
         forecastSearch(search);
 
@@ -26,3 +26,19 @@ function searchResults() {
     }
 }
 
+
+function currentWeather(search) {
+    var currentDay = document.getElementById("current-day");
+    currentDay.className = "";
+
+    var currentApi = `https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=${apiKey}&units=imperial`;
+    
+    fetch(currentApi)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(data){
+        var currentEl = document.getElementById("current");
+        currentEl.textContent = "";
+    })
+}
